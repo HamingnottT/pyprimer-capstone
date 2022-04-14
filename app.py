c5cd1046e0110909d9fcd4371b335e99f3577058
HamingnottT <Jnottingham836@yahoo.com>
2022-04-14 17:58:51 -0500

    Sports thread added

--- app.py
+++ app.py
@@ -62,3 +62,25 @@ def games():
     game_messages = Chats.query.filter_by(topic='games').order_by(Chats.id)
 
     return render_template("games.html", topics = TOPICS, messages= game_messages)
+
+
+@app.route("/sports", methods=["POST", "GET"])
+def sports():
+    if request.method == "POST":
+        chat_message = request.form.get("message")
+        username = "Jake"
+        topic = "sports"
+        new_chats = Chats(username="Jake", message=chat_message, topic="sports")
+        try:
+            # print(new_chats.username)
+            # print(new_chats.message)
+            db.session.add(new_chats)
+            db.session.commit()
+            print("worked")
+            return redirect("/sports")
+        except: 
+            return render_template("error.html", message = "There was an error adding the user" )
+    
+    game_messages = Chats.query.filter_by(topic='sports').order_by(Chats.id)
+
+    return render_template("sports.html", topics = TOPICS, messages= game_messages)
